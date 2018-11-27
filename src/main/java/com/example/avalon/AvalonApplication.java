fe3ae429ea9cae79ce1bcb316f6c48bf2c3abeb3
QiuChunMing <1148082901@qq.com>
2018-11-27 21:31:10 +0800

    Initial commit 初始版本提交

---
+++ src/main/java/com/example/avalon/AvalonApplication.java
@@ -0,0 +1,13 @@
+package com.example.avalon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Conditional;
+
+@SpringBootApplication
+    public class AvalonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AvalonApplication.class, args);
+    }
+}
