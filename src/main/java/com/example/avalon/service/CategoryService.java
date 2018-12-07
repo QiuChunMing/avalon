88c70c802254e57d67497129cc3012a458984fc8
QiuChunMing <1148082901@qq.com>
2018-12-07 22:21:04 +0800

    商店种类表搜索优化

--- src/main/java/com/example/avalon/service/CategoryService.java
+++ src/main/java/com/example/avalon/service/CategoryService.java
@@ -8,71 +8,80 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @Service
 @Slf4j
 public class CategoryService {
 
 
-    static final int ROOT_ID = 0;
-
     @Autowired
     private CategoryRepository categoryRepository;
 
     public ServiceResult<List<CategoryVO>> getCategories() {
+        List<Category> categoryList = getCategoriesList();
+        List<CategoryVO> categoryVOList = new ArrayList<>();
 
-        List<CategoryVO> categories = new ArrayList<>();
-
-        List<Category> all = categoryRepository.findAll();
-        Map<Integer, Map<Integer, List<Category>>> map = all.stream().collect(
-                Collectors.groupingBy(Category::getLevel,
-                        Collectors.groupingBy(Category::getParentId)));
-
-        int maxLevel = map.size();
-        List<Integer> ids = new ArrayList<>();
-        List<Category> list = map.get(1).get(0);
-        //添加第一个类别（总的类别）
-        CategoryVO firstCatogery = new CategoryVO();
-        firstCatogery.setName("全部商家");
-        firstCatogery.setIds(map.get(1).get(0).stream()
-                .map(Category::getId).collect(Collectors.toList()));
-        categories.add(firstCatogery);
-
-        //TODO
-        List<Category> mainCategories = map.get(1).get(ROOT_ID);
-
-
-        //对二级菜单根据父级菜单进行分类
-        Map<Integer, List<Category>> collect = map.get(2);
-
-        collect.forEach((i, j) -> log.debug("collect {} :{}", i, String.valueOf(j.size())));
-        //按照一级菜单Id,顺序取出分类后的二级菜单
-        for (Category mainCategory : mainCategories) {
-            CategoryVO subCategoryVO = new CategoryVO();
-            //设置Id
-            int mainCategoryId = mainCategory.getId();
-            subCategoryVO.setId(mainCategoryId);
-            subCategoryVO.setIds(Arrays.asList(mainCategoryId));
-
-            //将二级菜单Entity转换为VO
-            List<Category> subCategories = collect.get(mainCategoryId);
-            List<CategoryVO> subCategoriesVO = new ArrayList<>();
-            wrapCategoryToVO(subCategories, subCategoriesVO);
-
-            subCategoryVO.setSub_categories(subCategoriesVO);
-            categories.add(subCategoryVO);
-        }
-
-        return ServiceResult.of(categories);
+        wrapCategoryListToVOList(categoryList, categoryVOList);
+        return ServiceResult.of(categoryVOList);
     }
 
-    private void wrapCategoryToVO(List<Category> categories, List<CategoryVO> categoryVOS) {
-        for (Category category : categories) {
+    private void wrapCategoryListToVOList(List<Category> categoryList, List<CategoryVO> categoryVOList) {
+        log.debug("category string : {}", categoryList.toString());
+        log.debug("category string : {}", categoryVOList.toString());
+        for (Category category : categoryList) {
             CategoryVO categoryVO = new CategoryVO();
-            BeanUtils.copyProperties(category, categoryVO);
-            categoryVOS.add(categoryVO);
+            BeanUtils.copyProperties(category,categoryVO);
+            categoryVOList.add(categoryVO);
+            categoryVO.setSub_categories(new ArrayList<>());
+
+            if (category.getCategory() != null && category.getCategory().size() > 0) {
+                wrapCategoryListToVOList(category.getCategory(),categoryVO.getSub_categories());
+            }
         }
+    }
+
+
+
+    private List<Category> getCategoriesList() {
+        List<Category> allCategory = categoryRepository.findAll();
+
+        List<Category> rootCategory = new ArrayList<>();
+
+        //找出根种类
+        for (Category category : allCategory) {
+            if (category.getParentId() == 0) {
+                rootCategory.add(category);
+            }
+        }
+        //递归查找根种类下的次级种类
+        for (Category category : rootCategory) {
+            List<Category> subCategory = getSubCategory(category, allCategory);
+            category.setCategory(subCategory);
+        }
+
+        return rootCategory;
+    }
+
+    private List<Category> getSubCategory(Category category, List<Category> allCategory) {
+        //找出子类型
+        List<Category> subCatogories = new ArrayList<>();
+        for (Category category1 : allCategory) {
+            if (category.getId() == category1.getParentId()) {
+                subCatogories.add(category1);
+            }
+        }
+        //递归查找
+        for (Category subCatogory : subCatogories) {
+            category.setCategory(getSubCategory(subCatogory, allCategory));
+        }
+
+        if (subCatogories.size() == 0) {
+            return Collections.EMPTY_LIST;
+        }
+
+        return subCatogories;
     }
 }
